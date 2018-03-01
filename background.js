10f0cf9e4593c9180141fd1b6d0ce93f2c0046fc
Nikolaj <nikolaj.baer@gmail.com>
2018-03-01 14:05:40 -0800

    initial communication of headers working ,but not catching first page in time

--- background.js
+++ background.js
@@ -6,13 +6,26 @@ var header_names = {
     additional: ["Server,Via,Vary"]
 };
 
+function send_varnish_info(details,age){
+    chrome.tabs.sendMessage(details.tabId,{
+        "varnish_age":age,
+        "url":details.url,
+        "method":details.method
+    });
+}
+
 chrome.webRequest.onHeadersReceived.addListener(
     function(details) {
         chrome.runtime.sendMessage("reportVarnish", {"varnish_age":"X"});
 
         for(var k in details.responseHeaders){
-            if( k.name == header_names.age){
-                chrome.runtime.sendMessage("reportVarnish", {"varnish_age":k.value});
+            header = details.responseHeaders[k];
+            if( header.name == header_names.age){
+                var v = Number(header.value);
+                if(v > 0){
+                    console.log("recieved cached headers",details);
+                    send_varnish_info(details,v);
+                }
             }
         }
     }, {urls:["<all_urls>"]},  ["responseHeaders"]
--- contentscript.js
+++ contentscript.js
@@ -1,15 +1,20 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+        if(request.varnish_age != undefined){ 
+            console.log("Cached request (Aged "+request.varnish_age+"):",request.url);
+        }
+        /*
         if (request.requested == "reportVarnish"){
             //Code to create the div
             console.log("Received Event from extension",request);
-            addOverlayDiv();
+            //addOverlayDiv();
             sendResponse({confirmation: "Successfully received event"});
-        }
+        }*/
     }
 );
 console.log("added listener for reportVarnish");
 
+/*
 function addOverlayDiv(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(
@@ -24,5 +29,5 @@ function addOverlayDiv(){
         );
     });
 }
-
+*/
 
